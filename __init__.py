ee1541e5e0d3a7b2f239fa9065633a339d2c5c99
SpectralVectors <robmuir1@gmail.com>
2021-06-30 08:47:39 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
+bl_info = {
+    'name': 'Custom Objects',
+    'category': 'Object',
+    'author': 'Spectral Vectors',
+    'version': (0, 1, 0),
+    'blender': (2, 80, 0),
+    'location': 'Toolbar & Add Menu',
+    'description': 'Adds Meshes, Lights and Probes Scaled to 0.01 Unit Scale'
+}
+
 from .CustomObjects import *
